573c3e04ef39bf0915feba092d0e2bcb20e29b8b
MaxAlmeida + TomazMartins <llewxam100@hotmail.com>
2013-11-15 13:17:44 -0200

    criação da exceção NullCotaParlamentarException

--- src/com/OMM/application/user/exceptions/NullCotaParlamentarException.java
+++ src/com/OMM/application/user/exceptions/NullCotaParlamentarException.java
@@ -1,22 +1,19 @@
 package com.OMM.application.user.exceptions;
 
-import android.annotation.TargetApi;
 import java.util.Arrays;
 
-@TargetApi(18)
-public class NullParlamentarException extends Exception {
+public class NullCotaParlamentarException extends Exception {
 
 	private static final long serialVersionUID = 1L;
 
 	@Override
 	public String toString() {
-		return "NullParlamentarException [fillInStackTrace()="
+		return "NullCotaParlamentarException [fillInStackTrace()="
 				+ fillInStackTrace() + ", getMessage()=" + getMessage()
 				+ ", getLocalizedMessage()=" + getLocalizedMessage()
 				+ ", getStackTrace()=" + Arrays.toString(getStackTrace())
 				+ ", toString()=" + super.toString() + ", getCause()="
-				+ getCause() + ", getSuppressed()="
-				+ ", getClass()="
+				+ getCause() + ", getSuppressed()=" + ", getClass()="
 				+ getClass() + ", hashCode()=" + hashCode() + "]";
 	}
 
