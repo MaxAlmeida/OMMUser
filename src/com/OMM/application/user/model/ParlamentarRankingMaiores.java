6074038df534fcb59b6874e535389e86b4b269bb
TomazMartins + Thaiane <tomaz.unb@outlook.com>
2013-11-22 01:41:54 -0200

    Adição de novos atributos à classe ParlamentarRankingMaiores

--- src/com/OMM/application/user/model/ParlamentarRankingMaiores.java
+++ src/com/OMM/application/user/model/ParlamentarRankingMaiores.java
@@ -1,12 +1,19 @@
 package com.OMM.application.user.model;
 
-public class ParlamentarRankingMaiores
-{
+public class ParlamentarRankingMaiores {
+	
+	private int id;
+	
 	private double valorTotalAnual;
 	
 	private String nomeParlamentar;
 	
+	private String partido;
+	
+	private String uf;
+	
 	public ParlamentarRankingMaiores() {
+		// Empty Constructor
 	}
 
 	public double getValor() {
@@ -27,5 +34,45 @@ public class ParlamentarRankingMaiores
 	public void setNomeParlamentar(String nomeParlamentar) {
 		
 		this.nomeParlamentar = nomeParlamentar;
+	}
+
+	public int getId() {
+		
+		return id;
+	}
+
+	public void setId(int id) {
+		
+		this.id = id;
+	}
+
+	public double getValorTotalAnual() {
+		
+		return valorTotalAnual;
+	}
+
+	public void setValorTotalAnual(double valorTotalAnual)  {
+		
+		this.valorTotalAnual = valorTotalAnual;
+	}
+
+	public String getPartido() {
+		
+		return partido;
+	}
+
+	public void setPartido(String partido) {
+		
+		this.partido = partido;
+	}
+
+	public String getUf() {
+		
+		return uf;
+	}
+
+	public void setUf(String uf) {
+		
+		this.uf = uf;
 	}
 }
