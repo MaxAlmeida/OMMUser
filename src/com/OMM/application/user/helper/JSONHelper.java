5212a62c0425af6333f97a630141d2b072b9a31b
Thaiane + MaxAlmeida <thaiane.unb@hotmail.com>
2013-11-22 02:03:06 -0200

    Implementação do método listParlamentarRankingMaioresFromJSON na JSONHelper

--- src/com/OMM/application/user/helper/JSONHelper.java
+++ src/com/OMM/application/user/helper/JSONHelper.java
@@ -4,27 +4,43 @@ import java.util.List;
 
 import com.OMM.application.user.model.CotaParlamentar;
 import com.OMM.application.user.model.Parlamentar;
+import com.OMM.application.user.model.ParlamentarRankingMaiores;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
-public class JSONHelper {
-	public static List<Parlamentar> listParlamentarFromJSON(String jsonParlamentar) {
-		
+public class JSONHelper
+{
+	public static List<Parlamentar> listParlamentarFromJSON(
+			String jsonParlamentar) {
+
 		Gson gson = new Gson();
 		List<Parlamentar> listParlamentar = gson.fromJson(jsonParlamentar,
 				new TypeToken<List<Parlamentar>>() {
 				}.getType());
-		
+
 		return listParlamentar;
 	}
 
-	public static List<CotaParlamentar> listCotaParlamentarFromJSON(String jsonCotaParlamentar) {
+	public static List<CotaParlamentar> listCotaParlamentarFromJSON(
+			String jsonCotaParlamentar) {
+
+		Gson gson = new Gson();
+		List<CotaParlamentar> listCotaParlamentar = gson.fromJson(
+				jsonCotaParlamentar, new TypeToken<List<CotaParlamentar>>() {
+				}.getType());
+
+		return listCotaParlamentar;
+	}
+
+	public static List<ParlamentarRankingMaiores> listParlamentarRankingMaioresFromJSON(
+			String jsonParlamentarRankingMaiores) {
 		
 		Gson gson = new Gson();
-		List<CotaParlamentar> listCotaParlamentar = gson.fromJson(jsonCotaParlamentar,
-				new TypeToken<List<CotaParlamentar>>() {
+		List<ParlamentarRankingMaiores> listParlamentarRankingMaiores = gson.fromJson(
+				jsonParlamentarRankingMaiores, new TypeToken<List<ParlamentarRankingMaiores>>() {
 				}.getType());
 		
-		return listCotaParlamentar;
+		return listParlamentarRankingMaiores;
+
 	}
 }
