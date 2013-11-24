165092794211531d078a6e7e07ef5edbf68faf68
alysson-raniere <alysson_r076@hotmail.com>
2013-11-24 11:35:03 -0200

    Testes da model completos (100%) e JSONHelperTest concluído

--- test/com/OMM/test/user/helper/JSONHelperTest.java
+++ test/com/OMM/test/user/helper/JSONHelperTest.java
@@ -4,8 +4,6 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-import android.util.Log;
-
 import com.OMM.application.user.helper.JSONHelper;
 import com.OMM.application.user.model.CotaParlamentar;
 import com.OMM.application.user.model.Parlamentar;
@@ -14,25 +12,16 @@ public class JSONHelperTest {
 
 	@Test
 	public void testListParlamentarFromJSON() {
-		// Parlamentar parlamentar = new Parlamentar(337, "PAULO MALUF", "PP",
-		// "SP", 0, ));
+		JSONHelper jh = new JSONHelper();
 		Parlamentar parlamentar = new Parlamentar();
 		parlamentar.setNome("PAULO MALUF");
 		parlamentar.setId(373);
-		parlamentar.setPartido("PP");
-		parlamentar.setUf("SP");
-		// ArrayList<Parlamentar> list = new ArrayList<Parlamentar>();
-		// list.add(parlamentar);
-
-		assertTrue(parlamentar
-				.equals(JSONHelper
-						.listParlamentarFromJSON(
-								"[{\"id\":373,\"nome\":\"PAULO MALUF\",\"partido\":\"PP\",\"uf\":\"SP\"}]")
-						.get(0)));
-
-		// assertEquals(parlamentar.getNome()
-		// ,JSONHelper.listParlamentarFromJSON("[{\"id\":373,\"nome\":\"PAULO MALUF\",\"partido\":\"PP\",\"uf\":\"SP\"}]").get(0).getNome());
-	}
+		
+		Parlamentar p2 = JSONHelper.listParlamentarFromJSON("[{\"id\":373,\"nome\":\"PAULO MALUF\",\"partido\":\"PP\",\"uf\":\"SP\"}]").get(0);
+		
+		assertEquals(parlamentar.getId(), p2.getId());
+		assertTrue(parlamentar.getNome().equals(p2.getNome()));
+}
 
 	@Test
 	public void testListCotaParlamentarFromJSON() {
@@ -41,12 +30,12 @@ public class JSONHelperTest {
 		cota.setId(144068);
 		cota.setIdParlamentar(373);
 
-		cota = JSONHelper
+		CotaParlamentar cota2 = JSONHelper
 				.listCotaParlamentarFromJSON(
 						"[{\"cod\":144068,\"idParlamentar\":373,\"mes\":7,\"ano\":2013,\"numeroSubCota\":3,\"descricao\":\"COMBUST\",\"valor\":150.0}]")
 				.get(0);
 
-		// assertTrue(cota.equals(JSONHelper.listCotaParlamentarFromJSON("[{\"cod\":144068,\"idParlamentar\":373,\"mes\":7,\"ano\":2013,\"numeroSubCota\":3,\"descricao\":\"COMBUST\",\"valor\":150.0}]").get(0)));
+		assertEquals(cota.getId(), cota2.getId());
 
 	}
 
