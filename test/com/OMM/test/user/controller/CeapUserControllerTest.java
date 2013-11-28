a632cdd2dcfb6b592cf5f74c345ab1a5f9d76bed
MaxAlmeida + Thaiane <llewxam150@gmail.com>
2013-11-28 21:33:41 -0200

    Implementação PersistCota

--- test/com/OMM/test/user/controller/CeapUserControllerTest.java
+++ test/com/OMM/test/user/controller/CeapUserControllerTest.java
@@ -9,6 +9,7 @@ import android.test.ActivityInstrumentationTestCase2;
 
 import com.OMM.application.user.controller.CeapUserController;
 import com.OMM.application.user.dao.CotaParlamentarUserDao;
+import com.OMM.application.user.exceptions.NullParlamentarException;
 import com.OMM.application.user.helper.JSONHelper;
 import com.OMM.application.user.model.CotaParlamentar;
 import com.OMM.application.user.model.Parlamentar;
@@ -61,23 +62,23 @@ public class CeapUserControllerTest extends ActivityInstrumentationTestCase2<Gui
 		assertSame(controller1, controller2);
 	}
 //TODO Verificar teste no junit 3 com exceção
-/*
-public void testConvertJsonToCotaParlamentar(){
+
+/*public void testConvertJsonToCotaParlamentar(){
 		
 		String json = "[{\"cod\":144068,\"idParlamentar\":373,\"mes\":7,\"ano\":2013,\"numeroSubCota\":3,\"descricao\":\"COMBUST\",\"valor\":150.0}]";
 		List<CotaParlamentar> list = JSONHelper.listCotaParlamentarFromJSON(json);
 		
 		Assert.assertEquals(list, controller.convertJsonToCotaParlamentar(json));
 		
-	}
+	}*/
 
-public void testPersistCotaDB(){
+public void testPersistCotaDB() throws NullParlamentarException{
 	
 	Assert.assertTrue(controller.persistCotaDB(parlamentar));
 	
 	
 }
-*/
+
 public void deleteCota(){
 
 	Assert.assertFalse(controller.deleteCota(parlamentar));
