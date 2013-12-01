6a75d3f0500ad4ced757a3efe1568be6a3f30845
Rudmar + ViBorges <rudmar_junior@hotmail.com>
2013-12-01 04:33:13 -0200

    Refatoracao nomes da classe test e teste da NullCotaParlamentarException

---
+++ test/com/OMM/test/user/exceptions/NullParlamentarRankingMaioresExceptionTest.java
@@ -0,0 +1,14 @@
+package com.OMM.test.user.exceptions;
+
+import android.test.AndroidTestCase;
+
+public class NullParlamentarRankingMaioresExceptionTest extends AndroidTestCase {
+
+public void testInstance(){
+		
+		NullParlamentarRankingMaioresExceptionTest exception = new NullParlamentarRankingMaioresExceptionTest();
+		assertEquals(NullParlamentarRankingMaioresExceptionTest.class, exception.getClass());
+		
+	}
+		
+}
---
+++ test/com/OMM/test/user/exceptions/RequestFailedExceptionTest.java
@@ -0,0 +1,15 @@
+package com.OMM.test.user.exceptions;
+
+import android.test.AndroidTestCase;
+
+
+
+public class RequestFailedExceptionTest extends AndroidTestCase {
+public void testInstance(){
+		
+		RequestFailedExceptionTest exception = new RequestFailedExceptionTest();
+		assertEquals(RequestFailedExceptionTest.class, exception.getClass());
+		
+	}
+
+}
