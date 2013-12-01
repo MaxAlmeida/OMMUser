733696da62e7f6874c1afe4f691c0e7faf6919fd
ViBorges +Rudmar <vbda123@gmail.com>
2013-12-01 05:16:04 -0200

    Teste da classe nullParlmentarException

--- test/com/OMM/test/user/exceptions/NullParlamentarExceptionTest.java
+++ test/com/OMM/test/user/exceptions/NullParlamentarExceptionTest.java
@@ -1,6 +1,21 @@
 package com.OMM.test.user.exceptions;
 
-public class NullParlamentarExceptionTest {
+import android.test.AndroidTestCase;
 
+import com.OMM.application.user.model.Parlamentar;
+
+public class NullParlamentarExceptionTest extends AndroidTestCase {
+
+	@SuppressWarnings("null")
+	public void testNullParlamentarException() throws Exception {
+		try {
+			Parlamentar parlamentar = null;
+			parlamentar.toString();
+			fail("CotaParlamentarException is null");
+		} catch (NullPointerException e) {
+			assertTrue(true);
+		}
+
+	}
 
 }
