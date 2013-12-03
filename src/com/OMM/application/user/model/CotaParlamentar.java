ccb130dc1b8e203051b0112a586312e8fafb3def
Rudmar <rudmar_junior@hotmail.com>
2013-12-03 00:06:53 -0200

    Refatoração para padronização de código no pacote model

--- src/com/OMM/application/user/model/CotaParlamentar.java
+++ src/com/OMM/application/user/model/CotaParlamentar.java
@@ -1,94 +1,78 @@
 package com.OMM.application.user.model;
 
-public class CotaParlamentar
-{
+public class CotaParlamentar {
 
 	private int cod;
-	
+
 	private int idParlamentar;
-	
+
 	private int mes;
-	
+
 	private int ano;
-	
+
 	private int numeroSubCota;
-	
+
 	private String descricao;
-	
+
 	private double valor;
 
-	public CotaParlamentar()
-	{
+	public CotaParlamentar() {
 	}
 
-	public int getCod( )
-	{
+	public int getCod() {
 		return cod;
 	}
 
-	public void setCod( int cod )
-	{
+	public void setCod(int cod) {
 		this.cod = cod;
 	}
 
-	public int getIdParlamentar( )
-	{
+	public int getIdParlamentar() {
 		return idParlamentar;
 	}
 
-	public void setIdParlamentar( int idParlamentar )
-	{
+	public void setIdParlamentar(int idParlamentar) {
 		this.idParlamentar = idParlamentar;
 	}
 
-	public int getMes( )
-	{
+	public int getMes() {
 		return mes;
 	}
 
-	public void setMes( int mes )
-	{
+	public void setMes(int mes) {
 		this.mes = mes;
 	}
 
-	public int getAno( )
-	{
+	public int getAno() {
 		return ano;
 	}
 
-	public void setAno( int ano )
-	{
+	public void setAno(int ano) {
 		this.ano = ano;
 	}
 
-	public int getNumeroSubCota( )
-	{
+	public int getNumeroSubCota() {
 		return numeroSubCota;
 	}
 
-	public void setNumeroSubCota( int numeroSubCota )
-	{
+	public void setNumeroSubCota(int numeroSubCota) {
 		this.numeroSubCota = numeroSubCota;
 	}
 
-	public String getDescricao( )
-	{
+	public String getDescricao() {
 		return descricao;
 	}
 
-	public void setDescricao( String descricao )
-	{
+	public void setDescricao(String descricao) {
 		this.descricao = descricao;
 	}
 
-	public double getValor( )
-	{
+	public double getValor() {
 		return valor;
 	}
 
-	public void setValor( double valor )
-	{
+	public void setValor(double valor) {
 		this.valor = valor;
 	}
-	
+
 }
--- src/com/OMM/application/user/model/Parlamentar.java
+++ src/com/OMM/application/user/model/Parlamentar.java
@@ -97,5 +97,4 @@ public class Parlamentar {
 		this.foto = foto;
 	}
 
-
 }
