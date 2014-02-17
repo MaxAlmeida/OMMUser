04962c8e29503d51d76eea7e4c99515b35d8311e
TomazMartins <tomaz.unb@outlook.com>
2014-02-17 02:25:55 -0300

    Implementação do método onClick() de majorRankingListener

--- src/com/OMM/application/user/view/RankingListFragment.java
+++ src/com/OMM/application/user/view/RankingListFragment.java
@@ -3,15 +3,19 @@ package com.OMM.application.user.view;
 import com.OMM.application.user.R;
 
 import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class RankingListFragment extends Fragment {
+	
+	private static FragmentManager fragmentManager;
 
-	View majorRankingView = getActivity().findViewById(R.id.majorRanking_list);
-	View minorRankingView = getActivity().findViewById(R.id.minorRanking_list);
+	private View majorRankingView = getActivity().findViewById(R.id.majorRanking_list);
+	private View minorRankingView = getActivity().findViewById(R.id.minorRanking_list);
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -19,6 +23,8 @@ public class RankingListFragment extends Fragment {
 
 		View view = inflater.inflate(R.layout.fragment_list_rankings,
 				container, false);
+		
+		fragmentManager = this.getFragmentManager();
 		
 		majorRankingView.setOnClickListener(majorRankingListListener);
 		minorRankingView.setOnClickListener(minorRankingListListener);
@@ -30,8 +36,12 @@ public class RankingListFragment extends Fragment {
 		
 		@Override
 		public void onClick(View v) {
-			// TODO Auto-generated method stub
+
+			ParlamentarRankingListFragment parlamentarRankingListFragment = new ParlamentarRankingListFragment();
 			
+			FragmentTransaction transaction = fragmentManager.beginTransaction();
+			transaction.replace(R.id.fragment_container, parlamentarRankingListFragment);
+			transaction.commit();
 		}
 	};
 	
