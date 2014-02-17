c3db4e6eb65c30d347e42d7981770c02478a7c7a
TomazMartins <tomaz.unb@outlook.com>
2014-02-17 01:29:16 -0300

    Adição dos atributos majorRankingView e minorRankingView, além de seus OnClickListeners

--- src/com/OMM/application/user/view/RankingListFragment.java
+++ src/com/OMM/application/user/view/RankingListFragment.java
@@ -1,5 +1,7 @@
 package com.OMM.application.user.view;
 
+import com.OMM.application.user.R;
+
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -7,12 +9,38 @@ import android.view.View;
 import android.view.ViewGroup;
 
 public class RankingListFragment extends Fragment {
-	
+
+	View majorRankingView = getActivity().findViewById(R.id.majorRanking_list);
+	View minorRankingView = getActivity().findViewById(R.id.minorRanking_list);
+
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		
-		return container;
-	}
 
+		View view = inflater.inflate(R.layout.fragment_list_rankings,
+				container, false);
+		
+		majorRankingView.setOnClickListener(majorRankingListListener);
+		minorRankingView.setOnClickListener(minorRankingListListener);
+
+		return view;
+	}
+	
+	View.OnClickListener majorRankingListListener = new View.OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			
+		}
+	};
+	
+	View.OnClickListener minorRankingListListener = new View.OnClickListener() {
+		
+		@Override
+		public void onClick(View v) {
+			// TODO Auto-generated method stub
+			
+		}
+	}; 
 }
