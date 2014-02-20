5cce1360e377fc618b2a74de7c7d22682c9d23a7
MaxAlmeida <llewxam100@hotmail.com>
2014-02-20 15:53:48 -0300

    Adicionada ação para mostrar Ranking dos menores gastadores

--- src/com/OMM/application/user/view/RankingListFragment.java
+++ src/com/OMM/application/user/view/RankingListFragment.java
@@ -6,13 +6,14 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class RankingListFragment extends Fragment {
-	
+
 	private static FragmentManager fragmentManager;
 
 	private View majorRankingView;
@@ -24,44 +25,49 @@ public class RankingListFragment extends Fragment {
 
 		View view = inflater.inflate(R.layout.fragment_list_rankings,
 				container, false);
-		
+
 		fragmentManager = this.getFragmentManager();
-		
-		
+
 		return view;
 	}
-	
+
 	@Override
 	public void onResume() {
 		super.onResume();
 		majorRankingView = getActivity().findViewById(R.id.majorRanking_list);
 		minorRankingView = getActivity().findViewById(R.id.minorRanking_list);
-		
+
 		majorRankingView.setClickable(true);
 		majorRankingView.setOnClickListener(majorRankingListListener);
 		minorRankingView.setClickable(true);
 		minorRankingView.setOnClickListener(minorRankingListListener);
 	}
-	
+
 	View.OnClickListener majorRankingListListener = new View.OnClickListener() {
-		
+
 		@Override
 		public void onClick(View v) {
 
 			ParlamentarRankingListFragment parlamentarRankingListFragment = new ParlamentarRankingListFragment();
-			
-			FragmentTransaction transaction = fragmentManager.beginTransaction();
-			transaction.replace(R.id.fragment_container, parlamentarRankingListFragment);
+
+			FragmentTransaction transaction = fragmentManager
+					.beginTransaction();
+			transaction.replace(R.id.fragment_container,
+					parlamentarRankingListFragment);
 			transaction.commit();
 		}
 	};
-	
+
 	View.OnClickListener minorRankingListListener = new View.OnClickListener() {
-		
+
 		@Override
 		public void onClick(View v) {
-			// TODO Auto-generated method stub
-			
+			ParlamentarMenorListFragment parlamentarMenor = new ParlamentarMenorListFragment();
+			FragmentTransaction transaction = fragmentManager
+					.beginTransaction();
+			transaction.replace(R.id.fragment_container, parlamentarMenor);
+			transaction.commit();
+
 		}
-	}; 
+	};
 }
