32e0be08b0c0f452de5a897fd44cdd8fc40676bf
Thaiane <thaiane.unb@hotmail.com>
2014-03-01 21:03:24 -0300

    Corrigindo a exibição da posição dos parlamentares na pesquisa no ranqueamento

--- src/com/OMM/application/user/adapters/MajorRankingAdapter.java
+++ src/com/OMM/application/user/adapters/MajorRankingAdapter.java
@@ -49,7 +49,7 @@ public class MajorRankingAdapter extends ArrayAdapter<Parlamentar> {
 				+ valor.format(parlamentares.get(position).getValor()));
 		TextView textViewPosicao = (TextView) view
 				.findViewById(R.id.parlamentarlistfragment_txt_posicao);
-		textViewPosicao.setText("" + (position + 1));
+		textViewPosicao.setText("" + (parlamentares.get(position).getRanking() + 1));
 
 		return view;
 	}
