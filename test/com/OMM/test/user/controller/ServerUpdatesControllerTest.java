e1b5218eff2674ff8aaedf1c56e9041480a5dd7f
TomazMartins <tomaz.unb@outlook.com>
2014-04-20 09:29:43 -0300

    Consertando alguns métodos de teste da ServerUpdatesControllerTest

--- test/com/OMM/test/user/controller/ServerUpdatesControllerTest.java
+++ test/com/OMM/test/user/controller/ServerUpdatesControllerTest.java
@@ -1,23 +1,20 @@
 package com.OMM.test.user.controller;
 
-import com.OMM.application.user.controller.ServerUpdatesController;
-import com.OMM.application.user.dao.ServerUpdatesDao;
-
 import android.content.Context;
 import android.test.AndroidTestCase;
+
+import com.OMM.application.user.controller.ServerUpdatesController;
 
 public class ServerUpdatesControllerTest extends AndroidTestCase {
 
 	private Context context = null;
 	private ServerUpdatesController controller = null;
-	private ServerUpdatesDao dao = null;
 
 	public void setUp() throws Exception {
 		super.setUp();
 
 		this.context = getContext();
 		this.controller = ServerUpdatesController.getInstance(context);
-		this.dao = ServerUpdatesDao.getInstance(context);
 	}
 
 	public void testGetInstance() {
@@ -27,12 +24,13 @@ public class ServerUpdatesControllerTest extends AndroidTestCase {
 		assertSame(controller, testedController);
 	}
 	
-	public void testGetUrl() {
-		ServerUpdatesDao testedDao = ServerUpdatesDao.getInstance(context);
+	public void testGetAndInsertUrl() {
+		String testedUrl = "www.akan.com";
 		
-		String testedUrlHost = testedDao.getUrlServer();
-		String urlHostDao = dao.getUrlServer();
+		boolean testedResult = controller.insertUrlServer(testedUrl);
+		String url = controller.getUrl();
 		
-		assertEquals(urlHostDao, testedUrlHost);
+		assertEquals(true, testedResult);
+		assertEquals(testedUrl, url);
 	}
 }
