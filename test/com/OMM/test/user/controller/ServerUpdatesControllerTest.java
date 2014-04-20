b4d2c19ed64f84190c2986dfd91825526005f513
TomazMartins <tomaz.unb@outlook.com>
2014-04-20 09:09:15 -0300

    Implementando testGetUrl( ) na classe de teste da ServerUpdatesController

--- test/com/OMM/test/user/controller/ServerUpdatesControllerTest.java
+++ test/com/OMM/test/user/controller/ServerUpdatesControllerTest.java
@@ -1,6 +1,7 @@
 package com.OMM.test.user.controller;
 
 import com.OMM.application.user.controller.ServerUpdatesController;
+import com.OMM.application.user.dao.ServerUpdatesDao;
 
 import android.content.Context;
 import android.test.AndroidTestCase;
@@ -9,12 +10,14 @@ public class ServerUpdatesControllerTest extends AndroidTestCase {
 
 	private Context context = null;
 	private ServerUpdatesController controller = null;
+	private ServerUpdatesDao dao = null;
 
 	public void setUp() throws Exception {
 		super.setUp();
 
 		this.context = getContext();
 		this.controller = ServerUpdatesController.getInstance(context);
+		this.dao = ServerUpdatesDao.getInstance(context);
 	}
 
 	public void testGetInstance() {
@@ -22,5 +25,14 @@ public class ServerUpdatesControllerTest extends AndroidTestCase {
 				.getInstance(context);
 		
 		assertSame(controller, testedController);
+	}
+	
+	public void testGetUrl() {
+		ServerUpdatesDao testedDao = ServerUpdatesDao.getInstance(context);
+		
+		String testedUrlHost = testedDao.getUrlServer();
+		String urlHostDao = dao.getUrlServer();
+		
+		assertEquals(urlHostDao, testedUrlHost);
 	}
 }
